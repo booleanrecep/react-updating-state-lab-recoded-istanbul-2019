837fdb6c3d774c82de803359b0cbf120b7ccbf9b
Recep Öztürk <turkishteacher23@gmail.com>
2020-03-11 19:21:14 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
 // Code DigitalClicker Component Here
+import React from "react"
+class DigitalClicker  React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      timesClicked:0
+    }
+  }
+  
+  handleClick=()=>{
+    this.setState({
+      
+    })
+  }
+  render(){
+    return(
+      
+      <div>
+        <button onClick={}>Click</button>
+      </div>
+      
+      )
+  }
+}
